72e56661c6881fbe5c68262684256b632b88f3b0
Maxswell <maxswell.developer@gmail.com>
2022-08-11 05:15:45 -0500

    ft

---
+++ main.js
@@ -0,0 +1,39 @@
+// Pom ----------------------------------------
+
+function TocaSom(idElementoSom) {
+    const elemento = document.querySelector(idElementoSom);
+
+    if(elemento != null && elemento.localName === 'audio') {
+        
+        elemento.play();
+    } else {
+
+        console.log('Elemento não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i <= listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1]; 
+
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        TocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+              tecla.classList.add('ativa');  
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
